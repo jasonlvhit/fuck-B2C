d1be41e74bb728e62498f206e8a12e51f67813a7
jasonlvhit <jasonlvhit@gmail.com>
2014-06-21 20:53:49 +0800

    F***...

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+import os
 #configration
 DEBUG = True
 #DATABASE = '/.db'
 SECRET_KEY = '****'
 SQLALCHEMY_DATABASE_URI = 'sqlite:////home/jason/Flask/venv/B2C1/B2C.db'
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join('B2C/static/uploads')
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
